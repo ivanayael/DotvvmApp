8c2ca8a39b874e03ceb9b758ad74192316caf995
Ivana Yael Curra <icurra@quadiontech.com>
2020-07-16 17:54:10 -0300

    update code

---
+++ projectcode/DotvvmApp/DotvvmApp/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace DotvvmApp.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+		public double Number1 { get; set;}
+
+		public double Number2 { get; set; }
+
+		public double Result { get; set; }
+
+		public void Calculate(int precision)
+		{
+			Result = Math.Round(Number1 + Number2, precision);
+		}
+    }
+}
